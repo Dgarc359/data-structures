49f0405616e91fc73b1bf62cc94abf66171ac702
Dgarc359 <87497958+Dgarc359@users.noreply.github.com>
2022-07-30 15:45:04 -0400

    add makefile

---
+++ cpp/src/dynamic-array.cpp
@@ -0,0 +1,9 @@
+#include "dynamic-array.h"
+#include <iostream>
+using namespace std;
+
+template <typename T>
+
+void DynamicArray<T>::add(){ 
+    cout << "Dynamic Array Adding Elem";
+};
--- cpp/src/dynamic-array.h
+++ cpp/src/dynamic-array.h
@@ -5,17 +5,17 @@ template <typename T>
 class DynamicArray {
   public:
     int size;
-    void add() {
-
-
-      // check if adding elem to array overflows array size
-      // if it does, double array size
-      // otherwise add elem to array
-    };
+    /**
+     * @brief add an element to Dynamic Array.
+     * 
+     * Check if adding elem to array overflows array,
+     * if it does, double array size and then add element,
+     * otherwise add elem to array
+     */
+    void add(){};
 
   private:
     int maxSize;
-    
     /**
      * init inner array to size 16
      * hopefully we're doing generics right here... 
